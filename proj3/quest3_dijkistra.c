239124eb4f01c54c68c160a914850f26423f3927
carlosvale03 <dovalecarlos03@gmail.com>
2023-08-14 13:52:07 -0300

    up

--- proj3/quest3_dijkistra.c
+++ proj3/quest3_dijkistra.c
@@ -1,71 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <stdbool.h>
-#include <float.h>
 
-#define VG 4 // número de vértices no grafo
+#define V 5 // número de vértices no grafo
 
-void dijkstra(double graph[VG][VG], int V, int src, int dest) {
+int maxKey(double key[], bool mstSet[]) {
+    double max = 0;
+    int max_index;
+
+    for (int v = 0; v < V; v++)
+        if (mstSet[v] == false && key[v] > max)
+            max = key[v], max_index = v;
+
+    return max_index;
+}
+
+void dijkstra(double graph[V][V], int src, int dest) {
     double *dist = malloc(V * sizeof(double));
-    int *prev = malloc(V * sizeof(int));
-    bool *visited = malloc(V * sizeof(bool));
+    bool *sptSet = malloc(V * sizeof(bool));
+    int *parent = malloc(V * sizeof(int));
 
-    for (int i = 0; i < V; i++) {
-        dist[i] = DBL_MAX;
-        prev[i] = -1;
-        visited[i] = false;
+    for (int i = 0; i < V; i++){
+        dist[i] = 0;
+        sptSet[i] = false;
+        parent[i] = -1;
     }
 
-    dist[src] = 0;
+    dist[src] = 1;
 
     for (int count = 0; count < V - 1; count++) {
-        int u = -1;
-        for (int v = 0; v < V; v++) {
-            if (!visited[v] && (u == -1 || dist[v] < dist[u]))
-                u = v;
-        }
+        int u = maxKey(dist, sptSet);
+        sptSet[u] = true;
 
-        visited[u] = true;
-
-        for (int v = 0; v < V; v++) {
-            if (!visited[v] && graph[u][v] && dist[u] + graph[u][v] < dist[v]) {
-                dist[v] = dist[u] + graph[u][v];
-                prev[v] = u;
+        for (int v = 0; v < V; v++){
+            if (!sptSet[v] && graph[u][v] && dist[u] != 0 && dist[u] * graph[u][v] > dist[v]){
+                parent[v] = u;
+                dist[v] = dist[u] * graph[u][v];
             }
         }
     }
 
-    printf("Caminho mais confiavel: %d", src);
-    int current = dest;
-    while (current != -1) {
-        printf(" -> %d", current);
-        current = prev[current];
+    printf("Caminho mais confiavel: ");
+    int u = dest;
+    while(parent[u] != -1){
+        printf("%d <- ", u);
+        u = parent[u];
     }
-    printf("\n");
+    printf("%d\n", src);
 
     free(dist);
-    free(prev);
-    free(visited);
+    free(sptSet);
 }
 
 int main() {
-    double graph[VG][VG] = {{0, 0.3, 0.1, 0},
-                          {0.3, 0, 0.5, 0.9},
-                          {0.1, 0.5, 0, 0.2},
-                          {0, 0.9, 0.2, 0}};
+    double graph[V][V] = {{0, 0.3, 0.1, 0, 0},
+                          {0.3, 0, 0.5, 0.9, 0},
+                          {0.1, 0.5, 0, 0.2, 0},
+                          {0, 0.9, 0.2, 0, 1},
+                          {0,   0,   0,   1,   0}};
 
-    int src, dest;
-    while (1){
-        printf("Digite o vertice de origem: ");
-        scanf("%d", &src);
-        if(src==-1){
-            break;
-        }
-        printf("Digite o vertice de destino: ");
-        scanf("%d", &dest);
-
-        dijkstra(graph, VG, src, dest);
-    }
+    dijkstra(graph, 2, 4);
 
     return 0;
 }
