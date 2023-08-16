4fe0d93b4b45da023b74a69fb88af3b5340ca42a
carlosvale03 <dovalecarlos03@gmail.com>
2023-08-15 21:46:31 -0300

    Quebra de funções

--- proj3/quest1.c
+++ proj3/quest1.c
@@ -48,6 +48,17 @@
 
 
 
+void imprime_configuracoes(int **configuracoes, int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("Configuracao %d: ", i+1);
+        for (int j = 0; j < N; j++) {
+            printf("%d ", configuracoes[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
 // Função para converter um número na base 10 para a base 3
 void para_base3(int num, int *arr) {
     for (int i = 0; i < N; i++) {
@@ -56,65 +67,131 @@ void para_base3(int num, int *arr) {
     }
 }
 
+int mexeu(int **configuracoes, int i, int j)
+{
+    // Verifica se as duas configurações diferem em apenas uma posição
+    int diff = 0;
+    for (int k = 0; k < N; k++)
+    {
+        if (configuracoes[i][k] != configuracoes[j][k])
+        {
+            diff++;
+        }
+    }
+    return diff;
+}
+
+int encontra_disc_movido(int **configuracoes, int i, int j)
+{
+    // Encontra o disco que foi movido
+    int disco_movido;
+    for (int k = 0; k < N; k++)
+    {
+        if (configuracoes[i][k] != configuracoes[j][k])
+        {
+            disco_movido = k;
+            break;
+        }
+    }
+    return disco_movido;
+}
+
+int ehMenor(int disco_movido, int **configuracoes, int i)
+{
+    /**
+     * Verifica se um disco movido é o menor disco em relação à configuração atual.
+     *
+     * Esta função verifica se o disco movido é o menor disco no pino de origem em relação à configuração atual.
+     * Ela percorre os discos no pino de origem e compara com o disco movido para determinar se é o menor.
+     *
+     * @param disco_movido O índice do disco que foi movido.
+     * @param configuracoes Um array bidimensional que representa as configurações dos pinos.
+     * @param i O índice da configuração atual a ser verificada.
+     *
+     * @return 1 se o disco movido for o menor na configuração atual, 0 caso contrário.
+     */
+    int eh_menor = 1;
+    for (int k = 0; k < N; k++)
+    {
+        if (configuracoes[i][k] == configuracoes[i][disco_movido] && k < disco_movido)
+        {
+            eh_menor = 0;
+            break;
+        }
+    }
+    return eh_menor;
+}
+
+int mov_valido(int disco_movido, int **configuracoes, int j)
+{
+    /**
+     * Verifica se um movimento é válido, considerando o disco movido e a configuração de destino.
+     *
+     * Esta função verifica se um movimento é válido, considerando se o disco movido foi colocado em
+     * cima de um disco maior ou na base de um pino em relação à configuração de destino.
+     *
+     * @param disco_movido O índice do disco que foi movido.
+     * @param configuracoes Um array bidimensional que representa as configurações dos pinos.
+     * @param j O índice da configuração de destino a ser verificada.
+     *
+     * @return 1 se o movimento for válido, 0 caso contrário.
+     */
+    int eh_valido = 1;
+    for (int k = 0; k < N; k++)
+    {
+        if (configuracoes[j][k] == configuracoes[j][disco_movido] && k < disco_movido)
+        {
+            eh_valido = 0;
+            break;
+        }
+    }
+    return eh_valido;
+}
+
 // Função para criar a matriz de adjacência do grafo da Torre de Hanói
-int **cria_matriz_adjacencia() {
+int **cria_matriz_adjacencia()
+{
     // Aloca memória para a matriz de adjacência
     int tamanho = pow(3, N);
     int **matriz_adjacencia = malloc(tamanho * sizeof(int *));
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanho; i++)
+    {
         matriz_adjacencia[i] = calloc(tamanho, sizeof(int));
     }
 
     // Aloca memória para a lista de configurações
     int **configuracoes = malloc(tamanho * sizeof(int *));
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanho; i++)
+    {
         configuracoes[i] = malloc(N * sizeof(int));
         para_base3(i, configuracoes[i]);
     }
 
     // Cria as arestas do grafo
-    for (int i = 0; i < tamanho; i++) {
-        for (int j = 0; j < tamanho; j++) {
-            // Verifica se as duas configurações diferem em apenas uma posição
-            int diff = 0;
-            for (int k = 0; k < N; k++) {
-                if (configuracoes[i][k] != configuracoes[j][k]) {
-                    diff++;
-                }
-            }
+    for (int i = 0; i < tamanho; i++)
+    {
+        for (int j = 0; j < tamanho; j++)
+        {
+            int diff = mexeu(configuracoes, i, j);
             // Se diferem em apenas uma posição, verifica se o movimento é válido
-            if (diff == 1) {
-                // Encontra o disco que foi movido
-                int disco_movido;
-                for (int k = 0; k < N; k++) {
-                    if (configuracoes[i][k] != configuracoes[j][k]) {
-                        disco_movido = k;
-                        break;
-                    }
-                }
-                // Verifica se o disco movido é o menor disco no pino de origem
-                int eh_menor = 1;
-                for (int k = 0; k < N; k++) {
-                    if (configuracoes[i][k] == configuracoes[i][disco_movido] && k < disco_movido) {
-                        eh_menor = 0;
-                        break;
-                    }
-                }
-                // Verifica se o disco movido foi colocado em cima de um disco maior ou na base de um pino
-                int eh_valido = 1;
-                for (int k = 0; k < N; k++) {
-                    if (configuracoes[j][k] == configuracoes[j][disco_movido] && k < disco_movido) {
-                        eh_valido = 0;
-                        break;
-                    }
-                }
+            if (diff == 1)
+            {
+                int disco_movido = encontra_disc_movido(configuracoes, i, j);
+
+                int eh_menor = ehMenor(disco_movido, configuracoes, i);
+
+                int eh_valido = mov_valido(disco_movido, configuracoes, j);
+
                 // Se o movimento é válido, cria uma aresta entre as duas configurações
-                if (eh_menor && eh_valido) {
+                if (eh_menor && eh_valido)
+                {
                     matriz_adjacencia[i][j] = 1;
                 }
             }
         }
     }
+
+    imprime_configuracoes(configuracoes, tamanho);
 
     return matriz_adjacencia;
 }
@@ -136,6 +213,13 @@ int dijkstra(int **matriz_adjacencia, int *config_inicial, int *config_final) {
     for (int i = 0; i < N; i++) {
         indice_inicial += config_inicial[i] * pow(3, i);
     }
+
+    // exemplo:
+    // config_inicial = {1, 0, 2, 0}
+    // indice_inicial = 1 * 3^0 + 0 * 3^1 + 2 * 3^2 + 0 * 3^0
+    // indice_inicial = 1 + 0 + 18 + 0
+    // indice_inicial = 19
+
 
     // Converte a configuração final para um índice na matriz de adjacência
     int indice_final = 0;
--- proj3/quest3_dijkistra.c
+++ proj3/quest3_dijkistra.c
@@ -3,29 +3,74 @@
 #include <limits.h>
 #include <stdbool.h>
 
-#define V 5 // número de vértices no grafo
+#define V 4 // número de vértices no grafo
 
 int maxKey(double key[], bool mstSet[]) {
     double max = 0;
     int max_index;
 
     for (int v = 0; v < V; v++)
-        if (mstSet[v] == false && key[v] > max)
-            max = key[v], max_index = v;
+        if (mstSet[v] == false && key[v] > max){
+            max = key[v]; 
+            max_index = v;
+        }
 
     return max_index;
 }
 
-void dijkstra(double graph[V][V], int src, int dest) {
-    double *dist = malloc(V * sizeof(double));
-    bool *sptSet = malloc(V * sizeof(bool));
-    int *parent = malloc(V * sizeof(int));
+void imprm_camnh(int parent[], int j) {
+    /**
+     * Imprime o caminho de um vértice até o vértice de destino usando os pais calculados.
+     *
+     * Esta função imprime o caminho de um vértice até o vértice de destino usando os pais calculados.
+     * Ela utiliza recursão para imprimir o caminho na ordem correta.
+     *
+     * @param parent Um array que armazena os pais de cada vértice no caminho.
+     * @param j O vértice de destino para o qual o caminho será impresso.
+     */
+    if (parent[j] == -1)
+        return;
 
+    imprm_camnh(parent, parent[j]);
+
+    printf(" -> %d", j);
+}
+
+void iniclz_array(double *dist, bool *sptSet, int *parent){
+    /**
+     * Inicializa os arrays de distâncias, sptSet e parent com valores padrão.
+     *
+     * Esta função inicializa os arrays de distâncias, sptSet e parent com valores padrão.
+     * O array de distâncias é preenchido com zeros, o array sptSet é preenchido com valores booleanos "false" e o array parent é preenchido com -1.
+     *
+     * @param dist Um array para armazenar as distâncias.
+     * @param sptSet Um array booleano para rastrear se um vértice está incluído no conjunto SPT.
+     * @param parent Um array para armazenar os pais de cada vértice.
+     */
     for (int i = 0; i < V; i++){
         dist[i] = 0;
         sptSet[i] = false;
         parent[i] = -1;
     }
+}
+
+void dijkstra(double graph[V][V], int src, int dest) {
+    /**
+     * Executa o algoritmo de Dijkstra para encontrar o caminho mais confiável entre vértices em um grafo ponderado.
+     *
+     * Esta função utiliza o algoritmo de Dijkstra para encontrar o caminho mais confiável entre um vértice de origem (src) e um vértice de destino (dest)
+     * em um grafo ponderado representado por uma matriz de adjacência.
+     * O caminho mais confiável é calculado considerando os pesos das arestas.
+     *
+     * @param graph A matriz de adjacência representando o grafo ponderado.
+     * @param src O vértice de origem para iniciar o cálculo do caminho.
+     * @param dest O vértice de destino para o qual o caminho mais confiável será calculado.
+     */
+    double *dist = malloc(V * sizeof(double));
+    bool *sptSet = malloc(V * sizeof(bool));
+    int *parent = malloc(V * sizeof(int));
+
+    iniclz_array(dist, sptSet, parent);
 
     dist[src] = 1;
 
@@ -41,26 +86,22 @@ void dijkstra(double graph[V][V], int src, int dest) {
         }
     }
 
-    printf("Caminho mais confiavel: ");
-    int u = dest;
-    while(parent[u] != -1){
-        printf("%d <- ", u);
-        u = parent[u];
-    }
-    printf("%d\n", src);
+    printf("Caminho mais confiavel: %d", src);
+    imprm_camnh(parent, dest);
+    printf("\n");
 
     free(dist);
     free(sptSet);
+    free(parent);
 }
 
 int main() {
-    double graph[V][V] = {{0, 0.3, 0.1, 0, 0},
-                          {0.3, 0, 0.5, 0.9, 0},
-                          {0.1, 0.5, 0, 0.2, 0},
-                          {0, 0.9, 0.2, 0, 1},
-                          {0,   0,   0,   1,   0}};
+    double graph[V][V] = {{0, 0.3, 0.1, 0},
+                          {0.3, 0, 0.5, 0.9},
+                          {0.1, 0.5, 0, 0.2},
+                          {0, 0.9, 0.2, 0}};
 
-    dijkstra(graph, 2, 4);
+    dijkstra(graph, 2, 3);
 
     return 0;
 }
