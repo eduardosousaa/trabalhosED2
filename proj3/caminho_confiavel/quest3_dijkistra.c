fdf28b9cc9ea76bd346eef8265926ea2d2668075
carlosvale03 <dovalecarlos03@gmail.com>
2023-08-17 18:32:19 -0300

    Questão 3 finalizada

--- proj3/caminho_confiavel/quest3_dijkistra.c
+++ proj3/caminho_confiavel/quest3_dijkistra.c
@@ -4,114 +4,126 @@
 
 #define V 4 // número de vértices no grafo
 
-int maxKey(double key[], int mstSet[]) {
+int maior_chave(double chaves[], int arvMenrCamnh[])
+{
     /**
-     * Encontra o índice do vértice não incluído em um conjunto (mstSet) com a maior chave (peso).
+     * Encontra o vértice com a maior chave entre os vértices não incluídos na árvore mínima.
      *
-     * Esta função encontra o índice do vértice que ainda não foi incluído no conjunto mstSet
-     * e possui a maior chave (peso) entre os vértices não incluídos.
+     * Esta função percorre as chaves de todos os vértices não incluídos na árvore mínima (arvMenrCamnh)
+     * e encontra o vértice com a maior chave. Essa função é frequentemente usada em algoritmos de árvore geradora mínima,
+     * como o algoritmo de Prim, para selecionar o próximo vértice a ser adicionado à árvore.
      *
-     * @param key Um array que armazena as chaves (pesos) dos vértices.
-     * @param mstSet Um array que indica se um vértice está incluído em um conjunto (0 para não incluído, 1 para incluído).
-     *
-     * @return O índice do vértice com a maior chave (peso) entre os vértices não incluídos.
+     * @param chaves Um array que armazena as chaves (pesos) associadas a cada vértice.
+     * @param arvMenrCamnh Um array que indica quais vértices já estão na árvore mínima (0 para não incluídos, 1 para incluídos).
+     * @return O índice do vértice com a maior chave.
      */
     double max = 0;
-    int max_index;
+    int vertice_maior_peso;
 
     for (int v = 0; v < V; v++)
-        if (mstSet[v] == 0 && key[v] > max){
-            max = key[v]; 
-            max_index = v;
+        if (arvMenrCamnh[v] == 0 && chaves[v] > max)
+        {
+            max = chaves[v];
+            vertice_maior_peso = v;
         }
 
-    return max_index;
+    return vertice_maior_peso;
 }
 
-void imprm_camnh(int parent[], int j) {
+void imprm_camnh(int pai[], int j)
+{
     /**
-     * Imprime o caminho de um vértice até o vértice de destino usando os pais calculados.
+     * Imprime o caminho de um vértice até um destino utilizando informações de pais.
      *
-     * Esta função imprime o caminho de um vértice até o vértice de destino usando os pais calculados.
-     * Ela utiliza recursão para imprimir o caminho na ordem correta.
+     * Esta função imprime o caminho percorrido de um vértice até um destino, utilizando informações de pais.
+     * Ela utiliza recursão para percorrer o caminho na ordem inversa, indo do destino até o vértice inicial.
+     * O caminho é impresso em ordem correta, usando informações armazenadas nos pais de cada vértice.
      *
-     * @param parent Um array que armazena os pais de cada vértice no caminho.
+     * @param pai Um array que armazena os pais de cada vértice no caminho.
      * @param j O vértice de destino para o qual o caminho será impresso.
      */
-    if (parent[j] == -1)
+    if (pai[j] == -1)
         return;
 
-    imprm_camnh(parent, parent[j]);
+    imprm_camnh(pai, pai[j]);
 
     printf(" -> %d", j);
 }
 
-void iniclz_array(double *dist, int *sptSet, int *parent){
+void iniclz_array(double *dist, int *arvMenrCamnh, int *pai)
+{
     /**
-     * Inicializa os arrays de distâncias, sptSet e parent com valores padrão.
+     * Inicializa arrays de distâncias, árvore do menor caminho e pais com valores padrão.
      *
-     * Esta função inicializa os arrays de distâncias, sptSet e parent com valores padrão.
-     * O array de distâncias é preenchido com zeros, o array sptSet é preenchido com valores booleanos "false" e o array parent é preenchido com -1.
+     * Esta função inicializa os arrays de distâncias, árvore do menor caminho e pais com valores padrão.
+     * Ela percorre os arrays e atribui valores específicos a cada elemento. Os arrays são usados em algoritmos
+     * de busca de caminho em grafos.
      *
-     * @param dist Um array para armazenar as distâncias.
-     * @param sptSet Um array booleano para rastrear se um vértice está incluído no conjunto SPT.
-     * @param parent Um array para armazenar os pais de cada vértice.
+     * @param dist Um array de distâncias a ser inicializado.
+     * @param arvMenrCamnh Um array da árvore do menor caminho a ser inicializado.
+     * @param pai Um array de pais a ser inicializado.
      */
-    for (int i = 0; i < V; i++){
+    for (int i = 0; i < V; i++)
+    {
         dist[i] = 0;
-        sptSet[i] = 0;
-        parent[i] = -1;
+        arvMenrCamnh[i] = 0;
+        pai[i] = -1;
     }
 }
 
-void dijkstra(double graph[V][V], int src, int dest) {
+void dijkstra(double grafo[V][V], int origem, int dest)
+{
     /**
      * Executa o algoritmo de Dijkstra para encontrar o caminho mais confiável entre vértices em um grafo ponderado.
      *
-     * Esta função utiliza o algoritmo de Dijkstra para encontrar o caminho mais confiável entre um vértice de origem (src) e um vértice de destino (dest)
+     * Esta função utiliza o algoritmo de Dijkstra para encontrar o caminho mais confiável entre um vértice de origem (origem) e um vértice de destino (dest)
      * em um grafo ponderado representado por uma matriz de adjacência.
      * O caminho mais confiável é calculado considerando os pesos das arestas.
      *
-     * @param graph A matriz de adjacência representando o grafo ponderado.
-     * @param src O vértice de origem para iniciar o cálculo do caminho.
+     * @param grafo A matriz de adjacência representando o grafo ponderado.
+     * @param origem O vértice de origem para iniciar o cálculo do caminho.
      * @param dest O vértice de destino para o qual o caminho mais confiável será calculado.
      */
     double *dist = malloc(V * sizeof(double));
-    int *sptSet = malloc(V * sizeof(int));
-    int *parent = malloc(V * sizeof(int));
+    int *amcSet = malloc(V * sizeof(int)); // usada para setar a arvore de menor caminho (amc)
+    int *pai = malloc(V * sizeof(int));
 
-    iniclz_array(dist, sptSet, parent);
+    iniclz_array(dist, amcSet, pai);
 
-    dist[src] = 1;
+    dist[origem] = 1;
 
-    for (int count = 0; count < V - 1; count++) {
-        int u = maxKey(dist, sptSet);
-        sptSet[u] = 1;
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = maior_chave(dist, amcSet);
+        amcSet[u] = 1;
 
-        for (int v = 0; v < V; v++){
-            if (!sptSet[v] && graph[u][v] && dist[u] != 0 && dist[u] * graph[u][v] > dist[v]){
-                parent[v] = u;
-                dist[v] = dist[u] * graph[u][v];
+        for (int v = 0; v < V; v++)
+        {
+            if (!amcSet[v] && grafo[u][v] && dist[u] != 0 && dist[u] * grafo[u][v] > dist[v])
+            {
+                pai[v] = u;
+                dist[v] = dist[u] * grafo[u][v];
             }
         }
     }
 
-    printf("Caminho mais confiavel: %d", src);
-    imprm_camnh(parent, dest);
+    printf("Caminho mais confiavel: %d", origem);
+    imprm_camnh(pai, dest);
     printf("\n");
 
     free(dist);
-    free(sptSet);
-    free(parent);
+    free(amcSet);
+    free(pai);
 }
 
-int main() {
-    double graph[V][V] = {{0, 0.3, 0.1, 0},
+int main()
+{
+    double grafo[V][V] = {{0, 0.3, 0.1, 0},
                           {0.3, 0, 0.5, 0.9},
                           {0.1, 0.5, 0, 0.2},
                           {0, 0.9, 0.2, 0}};
 
-    dijkstra(graph, 2, 3);
+    dijkstra(grafo, 2, 3);
 
     return 0;
 }
